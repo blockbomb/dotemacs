7683750c5a17f525a5a91f4004f9da8594390d45
Brian Block <brian.block@diebold.com>
2016-02-02 14:04:02 -0500

    Adding recursive hidshow.
    I found someone who made a hide/show to hide all the leaves of a file,
    mainly like closing all trees,  it works somewhat but could use some
    refining.

--- init.el
+++ init.el
@@ -71,6 +71,42 @@
 (add-hook 'sh-mode-hook         'hs-minor-mode)
 (add-hook 'python-mode-hook     'hs-minor-mode)
 
+(defun hs-hide-leafs-recursive (minp maxp)
+  "Hide blocks below point that do not contain further blocks in region (MINP MAXP)."
+  (when (hs-find-block-beginning)
+    (setq minp (1+ (point)))
+    (funcall hs-forward-sexp-func 1)
+    (setq maxp (1- (point))))
+  (unless hs-allow-nesting
+    (hs-discard-overlays minp maxp))
+  (goto-char minp)
+  (let ((leaf t))
+    (while (progn
+	     (forward-comment (buffer-size))
+	     (and (< (point) maxp)
+		  (re-search-forward hs-block-start-regexp maxp t)))
+      (setq pos (match-beginning hs-block-start-mdata-select))
+      (if (hs-hide-leafs-recursive minp maxp)
+	  (save-excursion
+	    (goto-char pos)
+	    (hs-hide-block-at-point t)))
+      (setq leaf nil))
+    (goto-char maxp)
+    leaf))
+
+(defun hs-hide-leafs ()
+  "Hide all blocks in the buffer that do not contain subordinate blocks.  The hook `hs-hide-hook` is run; see run-hooks."
+  (interactive)
+  (hs-life-goes-on
+   (save-excursion
+     (message "Hiding blocks ...")
+     (save-excursion
+       (goto-char (point-min))
+       (hs-hide-leafs-recursive (point-min) (point-max)))
+     (message "Hiding blocks ... done"))
+   (run-hooks 'hs-hide-hook)))
+
+
 (require 'sgml-mode)
 (require 'nxml-mode)
 (add-to-list 'hs-special-modes-alist
